29550bd4e2f1d3195ba7b32ed41e98ef4222d1f9
abeytaadam <measalion@gmail.com>
2015-11-30 14:03:39 -0800

    deploying to heroku

--- public/scripts.js
+++ public/scripts.js
@@ -18,41 +18,41 @@ $(document).ready(function () {
 			event.preventDefault();
 			$hide.hide();
 			var genreInfo = {};
-			var genreName = $genreText.val(),
-					genreUrl = 'http://developer.echonest.com/api/v4/genre/profile?api_key=6SUIA2YM7GPJQQWZF&name=',
-					genreBucket = '&bucket=description&bucket=urls;',
-					artistUrl = 'http://developer.echonest.com/api/v4/genre/artists?api_key=6SUIA2YM7GPJQQWZF&format=json&results=10&bucket=hotttnesss&name=';
+			var genreName = $genreText.val();
+			// 		genreUrl = 'http://developer.echonest.com/api/v4/genre/profile?api_key=cookie&name=',
+			// 		genreBucket = '&bucket=description&bucket=urls;',
+			// 		artistUrl = 'http://developer.echonest.com/api/v4/genre/artists?api_key=cookie&format=json&results=10&bucket=hotttnesss&name=';
 
-			$.get(genreUrl + genreName.toLowerCase() + genreBucket, function (data) {
-				// Temporary append info
-				var genreAppend = data.response.genres;
+			// $.get(genreUrl + genreName.toLowerCase() + genreBucket, function (data) {
+			// 	// Temporary append info
+			// 	var genreAppend = data.response.genres;
 				
-				var genreData = data.response.genres[0];
-				genreInfo.genreName = genreData.name;
-				genreInfo.description = genreData.description;
-				genreInfo.urls = genreData.urls;
-				console.log('GENREINFO', genreInfo);
+			// 	var genreData = data.response.genres[0];
+			// 	genreInfo.genreName = genreData.name;
+			// 	genreInfo.description = genreData.description;
+			// 	genreInfo.urls = genreData.urls;
+			// 	console.log('GENREINFO', genreInfo);
 				
-				// Temporary append info
-				var html = template({
-							genres: genreAppend
-						});
-				$genreDescription.append(html);
-			});
-			console.log(genreName);
+			// 	// Temporary append info
+			// 	var html = template({
+			// 				genres: genreAppend
+			// 			});
+			// 	$genreDescription.append(html);
+			// });
+			// console.log(genreName);
 			
-			$.get(artistUrl + genreName.toLowerCase(), function (data) {
-				var artistData = data.response.artists;
-				var artistNames = [];
-				for (var i = 0; i < artistData.length; i++){
-					artistNames[i] = artistData[i].name;
-				}
-				genreInfo.artistNames = artistNames;
-				var html = template({
-							artists: artistData
-						});
-				$genreDescription.append(html);
-			});
+			// $.get(artistUrl + genreName.toLowerCase(), function (data) {
+			// 	var artistData = data.response.artists;
+			// 	var artistNames = [];
+			// 	for (var i = 0; i < artistData.length; i++){
+			// 		artistNames[i] = artistData[i].name;
+			// 	}
+			// 	genreInfo.artistNames = artistNames;
+			// 	var html = template({
+			// 				artists: artistData
+			// 			});
+			// 	$genreDescription.append(html);
+			// });
 
 			$.get('/api/genres/' + genreName, function (data) {
 				console.log('ROUTE RESPONSE', data);
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 // Require node packages
 var express = require('express'),
 		bodyParser = require('body-parser'),
+		request = require('request'),
 		mongoose = require('mongoose'),
-		hbs = require('hbs');
-		mongoose.connect('mongodb://localhost/delve-app');
+		hbs = require('hbs'),
+		dotenv = require('dotenv').load();
+		mongoose.connect(
+			process.env.MONGOLAB_URI || 
+			process.env.MONGOHQ_URL || 
+			'mongodb://localhost/delve-app');
+
 
 // Require genre model
 var Genre = require('./models/genre');
@@ -30,13 +36,41 @@ app.get('/', function (req, res) {
 // });
 
 app.get('/api/genres/:name', function (req, res) {
-	var name = req.params.name;
-	console.log(name);
-	Genre.find({genreName:name}, function (err, foundGenre) {
-		console.log('foundGenre', foundGenre);
+	var genreName = req.params.name,
+			genreUrl = 'http://developer.echonest.com/api/v4/genre/profile?api_key=' + process.env.api_key + '&name=',
+			genreBucket = '&bucket=description&bucket=urls;',
+			artistUrl = 'http://developer.echonest.com/api/v4/genre/artists?api_key=' + process.env.api_key + '&format=json&results=10&bucket=hotttnesss&name=';
+	var genreInfo = {};
 
-		res.json(foundGenre);
-	});
+	// Genre.find({genreName:genreName}, function (err, foundGenre) {
+	// 	console.log('foundGenre', foundGenre);
+	// 		if (foundGenre === []) {
+				request(genreUrl + genreName.toLowerCase() + genreBucket, function (error, res, genreBody) {
+					var genreData = JSON.parse(genreBody),
+							genreMore = genreData.response.genres[0];
+					
+					request(artistUrl + genreName.toLowerCase(), function (error, res, artistBody){
+						var artistData = JSON.parse(artistBody),
+							artistMore = artistData.response.artists;
+						artistNames = [];
+						for (var i = 0; i < artistMore.length; i++){
+							artistNames[i] = artistMore[i].name;
+						}
+						
+					genreInfo.name = genreMore.name;
+					genreInfo.description = genreMore.description;
+					genreInfo.artistNames = artistNames;
+					console.log(genreInfo);
+					});
+					
+				});
+				// console.log(genreInfo);
+				// request(artistUrl + genreName.toLowerCase()).pipe(res);
+				// console.log('RESPONSE',res);
+			// }
+	// 	console.log(foundGenre);
+	// 	res.json(foundGenre);
+	// });
 });
 
 // Render search results page
